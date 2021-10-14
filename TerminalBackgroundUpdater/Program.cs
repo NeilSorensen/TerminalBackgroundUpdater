d0fd2c5839c141d040383f9d359817071ef781a0
Neil Sorensen <nsorens@gmail.com>
2021-10-14 17:32:06 -0600

    Initial commit

---
+++ TerminalBackgroundUpdater/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.CommandLine;
+
+namespace TerminalBackgroundUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thing = new Argument<string>().FromAmong();
+            Console.WriteLine("Hello World!");
+            var rootCommand = new RootCommand
+            {
+                new Option<string>(aliases: new []{"-p", "--profile"}, description: "The terminal profile to change "),
+                new Option<string>()
+            };
+        }
+    }
+}
